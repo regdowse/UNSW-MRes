f686ecd46edafaa94945ad8d42c1dbcf652d3324
Reg Dowse <regdowse@gmail.com>
2026-08-11 11:05:24 +1000

    Use original 3D velocity orientation in surface fit

--- MRes/seacofs_eddy_dataset_modular/src/seacofs_eddy_dataset/stages/surface_fit.py
+++ MRes/seacofs_eddy_dataset_modular/src/seacofs_eddy_dataset/stages/surface_fit.py
@@ -11,7 +11,7 @@ from seacofs_eddy_dataset.core.doppio import bad_doppio_row, find_directional_ra
 from seacofs_eddy_dataset.core.esp import load_doppio_functions
 from seacofs_eddy_dataset.core.grid import fnumber_from_outer_avg, read_reference_grid
 from seacofs_eddy_dataset.core.velocity import rotate_uv
-from seacofs_eddy_dataset.core.vertical import interp_3d_to_reference_depths
+from seacofs_eddy_dataset.core.vertical import interp_3d_to_reference_depths, prepare_3d_velocity_field
 from seacofs_eddy_dataset.io import partition_path, write_partition
 from seacofs_eddy_dataset.stages.detection import find_model_files, reference_grid_path
 
@@ -273,8 +273,8 @@ def fit_surface_file(path: Path, config: PipelineConfig) -> pd.DataFrame:
             u_rot, v_rot = rotate_uv(u, v, grid.angle)
             vertical_check = None
             if require_vertical_profile:
-                u3d = np.transpose(dataset["u_eastward"][t, :, :, :], (2, 1, 0))
-                v3d = np.transpose(dataset["v_northward"][t, :, :, :], (2, 1, 0))
+                u3d = prepare_3d_velocity_field(dataset["u_eastward"][t, :, :, :])
+                v3d = prepare_3d_velocity_field(dataset["v_northward"][t, :, :, :])
                 vertical_check = {
                     "u_depth": interp_3d_to_reference_depths(u3d, z_r, target_depths),
                     "v_depth": interp_3d_to_reference_depths(v3d, z_r, target_depths),
